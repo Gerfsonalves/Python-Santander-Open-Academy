e09e7e0205a53947fd36de65682206a7b0ad1730
Gerfson Alves <gerfson17@hotmail.com>
2024-10-07 16:51:23 -0300

    first commit

---
+++ Santander.py
@@ -0,0 +1,33 @@
+# IF
+idade = 18
+
+
+if idade >= 18:
+    print("Você é maior de idade.")
+
+# IF-ELSE
+idade = 15
+
+
+if idade >= 18:
+    print("Você é maior de idade.")
+
+else:
+    print("Você é menor de idade.")
+
+
+# IF-ELIF-ELSE
+nota = 85
+
+
+if nota >= 90:
+    print("Excelente")
+
+elif nota >= 80:
+    print("Muito bom")
+
+elif nota >= 70:
+    print("Bom")
+
+else:
+    print("Precisa melhorar")
