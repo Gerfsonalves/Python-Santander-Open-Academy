5adfae2d16bbdf1ecfeccf8d217de5827162b06d
Gerfson Alves <gerfson17@hotmail.com>
2024-10-07 17:07:48 -0300

    Santander

--- Santander.py
+++ Santander.py
@@ -31,3 +31,49 @@ elif nota >= 70:
 
 else:
     print("Precisa melhorar")
+
+# Loops
+# For
+frutas = ["maçã", "banana", "laranja"]
+
+
+for fruta in frutas:
+    print(fruta)
+
+# While
+contador = 0
+
+
+while contador < 5:
+
+    print(contador)
+    contador += 1
+
+# Controle de loops
+# Break
+contador = 0
+
+
+while True:
+
+    print(contador)
+    contador += 1
+
+    if contador == 5:
+        break
+
+# Continue
+# Se o número for divisível por 2 (ou seja, se for par), a instrução continue é executada
+for i in range(10):
+
+    if i % 2 == 0:
+        continue
+    print(i)
+
+# Pass
+# A instrução pass é uma operação nula que não faz nada. É utilizada como um marcador de posição quando uma instrução é sintaticamente necessária, mas nenhuma ação é desejada.
+# A instrução pass é uma operação nula que não faz nada. É utilizada como um marcador de posição quando uma instrução é sintaticamente necessária, mas nenhuma ação é desejada.
+for i in range(5):
+    pass
+
+#
