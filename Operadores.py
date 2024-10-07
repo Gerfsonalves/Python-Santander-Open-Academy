69c43d1345338981ba28710e3937fe6fb080287e
Gerfson Alves <gerfson17@hotmail.com>
2024-10-07 17:18:06 -0300

    Operadores e Variaveis

---
+++ Operadores.py
@@ -0,0 +1,24 @@
+# Aritméticos
+a = 10
+b = 3
+
+
+soma = a + b   # 13
+subtracao = a - b    # 7
+multiplicacao = a * b    # 30
+divisao = a / b   # 3.333333333
+divisao_inteira = a // b   # 3
+modulo = a % b   # 1
+exponenciacao = a ** b   # 1000
+
+# De comparação
+a = 10
+b = 3
+
+
+igual = a == b   # False
+diferente = a != b   # True
+maiorQue = a > b   # True
+menoQue = a < b   # False
+maiorOUigual = a >= b   # True
+menorOUigual = a <= b   # False
---
+++ Variaveis.py
@@ -0,0 +1,12 @@
+# Declaração e atribuição de variáveis
+
+nome = "Juan"
+idade = 25
+altura = 1.75
+estudante = True
+
+print(nome, idade, altura, estudante)
+
+#
+a = b = c = 10
+print(b)
